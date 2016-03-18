3ed7f78a9da1266685d700daf568fd6d0809ff48
Nannalin Higgins <nhigg2@uis.edu>
2016-03-18 00:40:42 -0700

    Added links to index.html

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-// GET /style.css etc
-app.use(express.static(__dirname + '/public'));
-
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+// GET /style.css etc   static files static content
+app.use(express.static(__dirname + '/public'));
 
 app.get('/hello', function(req, res) {
   res.send('hello world from server.js again');
